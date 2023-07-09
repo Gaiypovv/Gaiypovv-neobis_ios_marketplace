50f104d7f6e5bc2c299f2c9353e1c8db5c2d8d27
Eldar <114824844+Gaiypovv@users.noreply.github.com>
2023-07-10 00:19:21 +0600

    Add PhoneNumberModel

---
+++ Neobis_IOS_ MobiMarket/NetworkLayer/Models/PhoneNumberModel.swift
@@ -0,0 +1,12 @@
+//
+//  PhoneNumberModel.swift
+//  Neobis_IOS_ MobiMarket
+//
+//  Created by Eldar Gaiypov on 10/7/23.
+//
+
+import Foundation
+
+struct PhoneNumberModel: Codable {
+    let phone_number: String
+}
