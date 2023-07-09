b5041fe502492e36791b4c0e9eb5533ab00c08aa
Eldar <114824844+Gaiypovv@users.noreply.github.com>
2023-07-10 00:15:17 +0600

    Add UnlikeModel

---
+++ Neobis_IOS_ MobiMarket/NetworkLayer/Models/UnlikeModel.swift
@@ -0,0 +1,12 @@
+//
+//  UnlikeModel.swift
+//  Neobis_IOS_ MobiMarket
+//
+//  Created by Eldar Gaiypov on 10/7/23.
+//
+
+import Foundation
+
+struct UnlikeModel: Codable {
+    let product_id: URL
+}
